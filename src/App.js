f35a49feec5231866f5cbee96be65c77336b0613
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-16 11:46:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false ')
+      .then(res => {
+        setCoins(res.data);
+       console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <h1 className="text"> Search </h1>
+        <form>
+          <input type="text" placeholder="Seach" className="input" />
+
+         
+        </form>
+      </div>
     </div>
   );
 }
