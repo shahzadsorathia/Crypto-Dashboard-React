5cf417ba68018ea2f468b3e1f88619a47b960285
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-17 15:57:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import './App.css';
 import Coin from './Coin';
 
-function App() {
-    {
-        return (
-            <div>
-                <Coin />
-            </div>
-        )
-    }
-}
 
-export default App;
+function App() {
+    return (
+      <div id="app">
+  
+        <NavBar />
+  
+        <Switch>
+          <Route path="/Coin">
+            <Coin />
+          </Route>
+  
+          <Route path="/Coin">
+            <Coin />
+          </Route>
+  
+          <Route path="/">
+            <Coin />
+          </Route>
+        </Switch>
+  
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default App;
