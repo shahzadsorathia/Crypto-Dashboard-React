b30f03e0211711c7179b629ff23f6498a03e5bfd
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-21 01:58:17 -0400

    Create header.jsx

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+ function Header ( ) {
+  return (
+    <header id='header'>
+      <div className='intro'>
+        <div className='overlay'>
+          <div className='container'>
+            <div className='col-md-8 col-md-offset-2 intro-text'>
+              <h1> Crytpo Price Tracker</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Header
