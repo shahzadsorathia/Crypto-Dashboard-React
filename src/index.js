98fa29d797ba9c4c4b713d8f6a8138f9dadd94a3
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-21 01:56:43 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  "./CSS/App.css"
-import App from './pages/App';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.unregister();
