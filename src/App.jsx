628d7f3c7bc8d666632b2630933cd56177ad1824
Shahzad Sorathia <shazadsorathia@gmail.com>
2021-09-21 01:58:13 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import  Navigation  from "./components/navigation";
+import Header  from "./components/header";
+import Coin from "./components/coin";
+import Contact from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      <Header />
+      <Coin  />
+     
+      <Contact data={landingPageData.Contact} />
+    </div>
+  );
+};
+
+export default App;
